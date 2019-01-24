9612f47520448d2902b22c1bc767d9e01110d033
Despres Mathis <s171162@ic2s114-08.univ-lemans.fr>
2019-01-24 16:00:28 +0100

    Commit from Univ

--- Exercice2_appro_pi.py
+++ Exercice2_appro_pi.py
@@ -2,9 +2,21 @@ import math
 import numpy as np 
 import matplotlib.pyplot as plt
 
-n = range(100000)
+n = 1000000
 somme = 0
-for i in n :
-    somme += np.sqrt(1-i*i)
+
+plt.title('Calcul de pi')
+plt.xlabel('Essai n°')
+plt.ylabel('Valeur')
+
+t=[]
+
+for i in range(1,n) :
+
+    x = np.random.rand()
+    somme += np.sqrt(1-x*x)
+    t.append(somme*4*(1/i))
+    plt.plot(t)
+    plt.pause(0.001)
 
 print(somme*4*(1/n))
